6d4d5065054395cbd42054163f20145e012c8356
Markiyan2 <153528986+Markiyan2@users.noreply.github.com>
2023-12-11 17:04:42 +0200

    Add files via upload

---
+++ Markian.py
@@ -0,0 +1,15 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import*
+
+app=QApplication([])
+win=QWidget()
+win.resize(500,500)
+win.setWindowTitle('Тест ГітХаб')
+
+title=QLabel('Тестовий додаток для гітХаб')
+line_h=QHBoxLayout(title)
+
+win.setLayout(line_h)
+
+win.show()
+app.exec_()
